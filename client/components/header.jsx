6f2638a703e8b35e2e799c1bb2b9129bf956f23f
Kieran Tallinger <dellingerkieran@gmail.com>
2020-03-02 20:58:35 -0800

    Created the Header class and set the return for its render method

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,2 +1,10 @@
 /* eslint-disable */
 import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <h1 className="header">Student Grade Table</h1>
+    )
+  }
+}
