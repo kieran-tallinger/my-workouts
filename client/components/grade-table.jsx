1ef1d5acf9d507227fc074a7ee72df3e04c8be65
Kieran Tallinger <dellingerkieran@gmail.com>
2020-03-02 21:04:29 -0800

    Imported the grade class from the grade module and created the GradeTable class with a constructor and initial state set

--- client/components/grade-table.jsx
+++ client/components/grade-table.jsx
@@ -1,2 +1,12 @@
 /* eslint-disable */
 import React, { Component } from 'react';
+import Grade from './grade'
+
+class GradeTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: null
+    }
+  }
+}
