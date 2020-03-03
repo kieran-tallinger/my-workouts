097a15d243cf73ecc6498a59a182d1b943164ab4
Kieran Tallinger <dellingerkieran@gmail.com>
2020-03-03 12:50:31 -0800

    Defined a GradeForm component in a new file and set up its contructor to have an initial state for the expected input fields

---
+++ client/components/grade-form.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+
+class GradeForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      course: '',
+      grade: ''
+    };
+  }
+}
