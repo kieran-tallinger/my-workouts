13d57d2c7c6c60631a9f9a1bfff57d74b0258d4d
Kieran Tallinger <dellingerkieran@gmail.com>
2020-03-03 11:59:28 -0800

    Modified the Header render to display an average grade badge with label based on the props it recieves

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
+  render(props) {
     return (
-      <h1 className="header">Student Grade Table</h1>
+      <header className='row py-3'>
+        <h1 className="col-8">Student Grade Table</h1>
+        <h2
+          className='col-4 text-right'>
+          Average Grade
+          <span className="px-2"></span>
+          <span className="badge badge-secondary px-4">
+            {props.average}
+          </span>
+        </h2>
+      </header>
     );
   }
 }
