fe4b6be3fddd5c7f43a118357bf3d267cd29ba21
Kieran Tallinger <dellingerkieran@gmail.com>
2020-03-03 16:24:20 -0800

    Added a new react element button to the Grade components render and styled it with bootstrap

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -6,6 +6,7 @@ function Grade(props) {
       <td>{props.name}</td>
       <td>{props.course}</td>
       <td>{props.grade}</td>
+      <button onClick={props.delete} className='btn btn-danger mx-1'>DELETE</button>
     </tr>
   );
 }
