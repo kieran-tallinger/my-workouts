de5cbae27f073dc5499ad46f0c9d62ca84b62d88
Kieran Tallinger <dellingerkieran@gmail.com>
2020-04-22 15:40:39 -0700

    Added methods for handling form value changes being applied to the component state

--- client/components/routine-form.jsx
+++ client/components/routine-form.jsx
@@ -10,6 +10,24 @@ class RoutineForm extends Component {
     };
   }
 
+  handleNameChange(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handleDescriptionChange(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  handleDifficultyChange(e) {
+    this.setState({
+      difficulty: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
