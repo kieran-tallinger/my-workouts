534c3f15a4ec938e1128bd43c1f7fd30718a8764
Kieran Tallinger <dellingerkieran@gmail.com>
2020-04-22 15:37:49 -0700

    Created a component called RoutineForm and gave it an initial state and render

---
+++ client/components/routine-form.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class RoutineForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      difficulty: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default RoutineForm;
