f52449780ef5e855269a55d99aff7f0516b89ebb
Kieran Tallinger <dellingerkieran@gmail.com>
2020-04-24 16:16:54 -0700

    Added select icon to the routine component and styled it with css

--- client/components/routines.jsx
+++ client/components/routines.jsx
@@ -8,13 +8,13 @@ function Routine(props) {
       <td>{props.difficulty}</td>
       <td>
         <a onClick={() => props.select(props.id)}>
-          <i></i>
+          <i className='fas fa-dumbbell fa-lg'></i>
         </a>
         <a onClick={() => props.update(props.id)}>
-          <i className='fas fa-edit fa-lg mr-3'></i>
+          <i className='fas fa-edit fa-lg mx-1'></i>
         </a>
         <a onClick={() => props.delete(props.id)}>
-          <i className='fas fa-trash-alt fa-lg ml-3'></i>
+          <i className='fas fa-trash-alt fa-lg'></i>
         </a>
       </td>
     </tr>
