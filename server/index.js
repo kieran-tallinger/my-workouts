6250527ffedb478e074e0646687e321c4953c395
Kieran Tallinger <dellingerkieran@gmail.com>
2020-04-27 16:36:22 -0700

    Put the require for dotenv in the correct place

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const path = require('path');
 const jsonServer = require('json-server');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-require('dotenv/config');
 const path = require('path');
 
 const srcPath = path.resolve(__dirname, 'client');
