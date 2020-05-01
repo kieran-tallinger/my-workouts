03ae7df7ca3f82eeb2a988fbc57c49f80bdbd5c5
Kieran Tallinger <dellingerkieran@gmail.com>
2020-05-01 16:33:25 -0700

    Added endpoint for getting all routines

--- server/index.js
+++ server/index.js
@@ -33,6 +33,21 @@ app.get('/api/exercises', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/routines', (req, res, next) => {
+  const sql = `
+    select "routineId",
+           "name",
+           "description",
+           "difficulty"
+      from "routines"
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => next(err));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
