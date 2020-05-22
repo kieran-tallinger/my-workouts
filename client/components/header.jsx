76df4355b4e74a9fe7d5a87877369b364f7a6d1a
Kieran Tallinger <dellingerkieran@gmail.com>
2020-05-22 15:38:42 -0700

    Centered view all exercises button for mobile portrait view

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ function Header(props) {
   return (
     <header className='row py-3'>
       <h1 className="col-md-8 text-center text-sm-center text-md-center text-lg-left">My Personal Workouts</h1>
-      <div className='col-md-4 text-sm-center text-lg-right'>
+      <div className='col-md-4 text-center text-md-right'>
         <button className='btn btn-info' onClick={() => props.toExercise()}>View My Exercises</button>
       </div>
     </header>
